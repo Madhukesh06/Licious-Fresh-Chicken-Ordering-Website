c09010642d9251ae16ea4f0172275f37e5af3fb8
Madhukesh K T <madhukeshthakur@gmail.com>
2022-08-26 01:57:03 +0530

    Project created

---
+++ handy-bubble-5100/src/App.js
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import "./App.css";
+import MainNavbar from "./Components/MainNavbar";
+import MiniNavbar from "./Components/MiniNavbar";
+
+function App() {
+  return (
+    <Box className="App">
+      {/* <MiniNavbar />
+      <MainNavbar /> */}
+    </Box>
+  );
+}
+
+export default App;
