bd04f21edc7a8c791faea9a2ec4e5c0bc040742c
Madhukesh K T <madhukeshthakur@gmail.com>
2022-08-28 10:25:12 +0530

    Footer added

--- handy-bubble-5100/src/App.js
+++ handy-bubble-5100/src/App.js
@@ -1,13 +1,15 @@
 import { Box } from "@chakra-ui/react";
 import "./App.css";
-import MainNavbar from "./Components/MainNavbar";
-import MiniNavbar from "./Components/MiniNavbar";
+import Footer from "./Components/Footer";
+// import MainNavbar from "./Components/MainNavbar";
+// import MiniNavbar from "./Components/MiniNavbar";
 
 function App() {
   return (
     <Box className="App">
       {/* <MiniNavbar />
       <MainNavbar /> */}
+      <Footer />
     </Box>
   );
 }
